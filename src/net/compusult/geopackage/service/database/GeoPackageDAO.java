de1a8b300df8b9a2d0adec1d1c25736760bf188b
Sean Hogan <sean@compusult.net>
2013-11-28 16:42:14 -0330

    Added license information (Apache 2.0).

--- src/net/compusult/geopackage/service/database/GeoPackageDAO.java
+++ src/net/compusult/geopackage/service/database/GeoPackageDAO.java
@@ -1,3 +1,21 @@
+/*
+ * GeoPackageDAO.java
+ * 
+ * Copyright 2013, Compusult Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+   
 package net.compusult.geopackage.service.database;
 
 import java.io.BufferedReader;
@@ -33,12 +51,6 @@ import net.compusult.geopackage.service.model.Rectangle;
 
 import org.apache.log4j.Logger;
 import org.springframework.util.StringUtils;
-
-/**
- * This DAO class is intentionally simple, given that its purpose is
- * just to help with certain data population programs.  For example,
- * a single connection is adequate for this.
- */
 
 public class GeoPackageDAO {	
 	private final static Logger LOG = Logger.getLogger(GeoPackageDAO.class);
